051670e8168bb6793f0516e2b510d714b1545ed5
Richard Holaj <holajr22@gmail.com>
2021-02-08 11:46:37 +0100

    new editor font (has to be imported to page where we use editor too)

--- src/main/ts/assets/style.css.ts
+++ src/main/ts/assets/style.css.ts
@@ -1,4 +1,8 @@
 export const cssMainStylesheet = `
+    @import url('https://fonts.googleapis.com/css2?family=Varta&display=swap');
+
+    body { font-family: Varta; }
+    
     @keyframes processing {
         0% {opacity: 1;}
         100% {opacity: 0;}
