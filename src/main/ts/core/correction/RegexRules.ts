b74461a15ec5dcfd5047a6ab2b9a9d5f7c5baeee
Richard Holaj <holajr22@gmail.com>
2021-03-08 12:40:23 +0100

    typography rules update

--- src/main/ts/core/correction/RegexRules.ts
+++ src/main/ts/core/correction/RegexRules.ts
@@ -4,13 +4,15 @@ export const cislice: string = '[0123456789]';
 export const mezery: string = '[\u{0020}\u{00A0}\u{1680}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{202F}\u{205F}\u{3000}]';
 export const pismena: string = '[a-zA-ZáčďéěíňóřšťúůýžÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ]';
 export const alnum: string = '[a-zA-ZáčďéěíňóřšťúůýžÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ0-9]';
-export const malapismena: string = '[a-záčďéěíňóřšťúůýž]';
+export const mala_pismena: string = '[a-záčďéěíňóřšťúůýž]';
+export const velka_pismena: string = '[A-ZÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ]';
 export const interpunkce: string = '[\\.,;:!\\?\u{2013}\u{2014}\u{2026}\u{0028}\u{0029}\u{005B}\u{005D}\u{007B}\u{007D}\00AB\u{00BB}\u{201E}\u{201C}\u{201A}\u{2018}]';
-export const interpunkcezaviraci: string = '[\u{0029}\u{005D}\u{007D}\u{00BB}\u{2018}\u{201C}]';
-export const interpunkceotviraci: string = '[\u{0028}\u{005B}\u{007B}\u{00AB}\u{201A}\u{201E}]';
+export const interpunkce_zaviraci: string = '[\u{0029}\u{005D}\u{007D}\u{00AB}\u{2018}\u{201C}]';
+export const interpunkce_otviraci: string = '[\u{0028}\u{005B}\u{007B}\u{00BB}\u{201A}\u{201E}]';
 export const apostrof: string = '[\u{02BC}\u{0027}\u{0060}\u{2018}\u{2019}\u{00B4}]';
 export const uvozovky: string = '[\u{0022}\u{0027}\u{0060}\u{2018}\u{2019}\u{201C}\u{201D}\u{00AB}\u{00B4}\u{2039}]';
-export const zacatek_slova: string = '[\u{0020}\u{00A0}\u{1680}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{202F}\u{205F}\u{3000}]';
+export const zacatek_slova: string = '[\u{0020}\u{00A0}\u{1680}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{202F}\u{205F}\u{3000}\u{0028}\u{005B}\u{007B}\u{00AB}\u{201A}\u{201E}]';
+export const konec_slova: string = '[\u{0020}\u{00A0}\u{1680}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{2007}\u{2008}\u{2009}\u{200A}\u{202F}\u{205F}\u{3000}\u{0029}\u{005D}\u{007D}\u{00BB}\u{2018}\u{201C}]';
 
 export let autocorrectRegexRules: RegexRule[] = [
     {
@@ -22,8 +24,8 @@ export let autocorrectRegexRules: RegexRule[] = [
     },
     {
         name: 'nezlomitelné mezery po jednopísmenných předložkách',
-        description: 'Použijte nerozdělitelnou mezeru',
-        correctionLabel: 'Opravte obyčejnou mezeru na nezlomitelnou (za předložkou, zkratkou, …)',
+        description: 'Použijte pevnou mezeru',
+        correctionLabel: 'Opravte obyčejnou mezeru na pevnou (za předložkou, zkratkou, …)',
         about:[{
             url: 'IJP',
             label: 'https://prirucka.ujc.cas.cz/?id=880'
@@ -33,24 +35,24 @@ export let autocorrectRegexRules: RegexRule[] = [
     },
     {
         name: 'nezlomitelné mezery po iniciálách a jednopísmenných zkratkách s tečkou',
-        description: 'Použijte nerozdělitelnou mezeru',
-        correctionLabel: 'Opravte obyčejnou mezeru na nezlomitelnou (za předložkou, zkratkou, …)',
+        description: 'Použijte pevnou mezeru',
+        correctionLabel: 'Opravte obyčejnou mezeru na pevnou (za předložkou, zkratkou, …)',
         about:[{
             url: 'IJP',
             label: 'https://prirucka.ujc.cas.cz/?id=880'
         }],
-        search: new RegExp('(' + zacatek_slova + pismena + '\\.)\u{0020}?=' + alnum , 'g'),
+        search: new RegExp('(' + zacatek_slova + pismena + '\\.)\u{0020}(?=' + alnum + ')', 'g'),
         replace: '$1\u{00A0}'
     },
     {
         name: 'nezlomitelné mezery po řadových číslovkých vyjádřených číslicí',
-        description: 'Použijte nerozdělitelnou mezeru',
-        correctionLabel: 'Opravte obyčejnou mezeru na nezlomitelnou (za předložkou, zkratkou, …)',
+        description: 'Použijte pevnou mezeru',
+        correctionLabel: 'Opravte obyčejnou mezeru na pevnou (za předložkou, zkratkou, …)',
         about:[{
             url: 'IJP',
             label: 'https://prirucka.ujc.cas.cz/?id=880'
         }],
-        search: new RegExp('(' + cislice + '{1,2}\\.)\u{0020}?=' + malapismena , 'g'),
+        search: new RegExp('(' + cislice + '{1,2}\\.)\u{0020}(?=' + mala_pismena + ')', 'g'),
         replace: '$1\u{00A0}'
     },
     {
@@ -68,7 +70,7 @@ export let autocorrectRegexRules: RegexRule[] = [
             url: 'https://prirucka.ujc.cas.cz/?id=160#nadpis10',
             label: 'IJP'
         }],
-        search: new RegExp('([' + interpunkcezaviraci + alnum + ']\\.)?=' + alnum , 'g'),
+        search: new RegExp('([' + interpunkce_zaviraci + alnum + ']\\.)(?=' + alnum + ')', 'g'),
         replace: '$1\u{0020}'
     },
     {
@@ -85,7 +87,7 @@ export let autocorrectRegexRules: RegexRule[] = [
             url: 'https://prirucka.ujc.cas.cz/?id=786',
             label: 'IJP'
         }],
-        search: new RegExp('(' + cislice + '{1,3})\u{0020}|\\.?=' + cislice + '{3}[^' + cislice + ']' , 'g'),
+        search: new RegExp('(' + cislice + '{1,3})\u{0020}|\\.(?=' + cislice + '{3}[^' + cislice + '])' , 'g'),
         replace: '$1\u{00A0}'
     },
     {
@@ -110,7 +112,7 @@ export let autocorrectRegexRules: RegexRule[] = [
     {
         name: 'nadbytečná mezera před otazníkem',
         description: 'Nadbytečná mezera před otazníkem',
-        search: new RegExp('\u{0020}\\?(\u{0020}|(' + interpunkcezaviraci + '))' , 'g'),
+        search: new RegExp('\u{0020}\\?(\u{0020}|(' + interpunkce_zaviraci + '))' , 'g'),
         replace: '?$1'
     },
     {
@@ -127,7 +129,7 @@ export let autocorrectRegexRules: RegexRule[] = [
     {
         name: 'nadbytečná mezera před vykřičníkem',
         description: 'Nadbytečná mezera před vykřičníkem',
-        search: new RegExp('\u{0020}!(\u{0020}|(' + interpunkcezaviraci + '))' , 'g'),
+        search: new RegExp('\u{0020}!(\u{0020}|(' + interpunkce_zaviraci + '))' , 'g'),
         replace: '!$1'
     },
     {
@@ -175,6 +177,12 @@ export let autocorrectRegexRules: RegexRule[] = [
         search: new RegExp('(' + cislice + '+)-?((ti)|(mi))', 'g'),
         replace: '$1'
     },
+	{
+        name: 'nesprávný zápis složených adjektiv a adverbií',
+        description: 'Nesprávný zápis složených adjektiv a adverbií',
+        search: new RegExp('([' + cislice + ']+)-([' + pismena + ']+)([(ý)(ého)(ému)(ého)(ém)(ým)(á)(é)(ou)(í)(ých)(ým)(ými)(ího)(ímu)(ím)(ích)(ími)(ě)(e)])', 'g'),
+        replace: '$1$2$3'
+    },
     {
         name: 'nesprávný a/nebo nesprávně umístěný apostrof u letopočtu',
         description: 'Nesprávný znak ',
@@ -183,7 +191,7 @@ export let autocorrectRegexRules: RegexRule[] = [
             url: 'https://prirucka.ujc.cas.cz/?id=168',
             label: 'IJP'
         }],
-        search: new RegExp(apostrof + '\u{0020}?(' + cislice + '{2})?=[^' + cislice + ']', 'g'),
+        search: new RegExp(apostrof + '\u{0020}?(' + cislice + '{2})(?=[^' + cislice + '])', 'g'),
         replace: '\u{02BC}$1'
     },
     {
@@ -194,7 +202,7 @@ export let autocorrectRegexRules: RegexRule[] = [
             url: 'https://prirucka.ujc.cas.cz/?id=168',
             label: 'IJP'
         }],
-        search: new RegExp('(' + pismena + ')\u{0020}?' + apostrof + '\u{0020}??=' + pismena, 'g'),
+        search: new RegExp('(' + pismena + ')\u{0020}?' + apostrof + '\u{0020}?(?=' + pismena + ')', 'g'),
         replace: '$1\u{02BC}'
     },
     {
@@ -237,21 +245,21 @@ export let autocorrectRegexRules: RegexRule[] = [
     {
         name: 'chybějící nezlomitelné mezery před znakem & a po něm u spojení slov',
         description: 'Nesprávná mezera',
-        correctionLabel: 'Změňte běžnou mezislovní mezeru na nerozdělitelnou',
+        correctionLabel: 'Změňte běžnou mezislovní mezeru na pevnou',
         search: new RegExp('(' + pismena + '{2,})\u{0020}?&\u{0020}?(' + pismena + '{2,})', 'g'),
         replace: '$1\u{0020}&\u{0020}$2'
     },
     {
         name: 'nadbytečné mezery před znakem & a po něm u spojení písmen',
         description: 'Nesprávná mezera',
-        correctionLabel: 'Změňte běžnou mezislovní mezeru na nerozdělitelnou',
-        search: new RegExp('(^' + pismena + ')\u{0020}?&\u{0020}?(' + pismena + '$)', 'g'),
+        correctionLabel: 'Změňte běžnou mezislovní mezeru na pevnou',
+        search: new RegExp('(^' + pismena + ')\u{0020}?&\u{0020}?(' + pismena + konec_slova + ')', 'g'),
         replace: '$1&$2'
     },
     {
         name: 'chybějící nezlomitelné mezery po znaménkách narození a úmrtí',
         description: 'Nesprávná mezera',
-        correctionLabel: 'Změňte běžnou mezislovní mezeru na nerozdělitelnou',
+        correctionLabel: 'Změňte běžnou mezislovní mezeru na pevnou',
         search: new RegExp('(\\*|†)\u{0020}?(?=' + cislice + ')' , 'g'),
         replace: '$1\u{00A0}'
     },
@@ -263,9 +271,9 @@ export let autocorrectRegexRules: RegexRule[] = [
         replace: '$1'
     },
     {
-        name: 'nezlomitelná mezera po znaku ⌀',
+        name: 'nezlomitelná mezera po znaku průměr',
         description: 'Nesprávná mezera',
-        correctionLabel: 'Změňte běžnou mezislovní mezeru na nerozdělitelnou',
+        correctionLabel: 'Změňte běžnou mezislovní mezeru na pevnou',
         about:[{
             url: 'https://prirucka.ujc.cas.cz/?id=785#nadpis3',
             label: 'IJP'
@@ -274,8 +282,8 @@ export let autocorrectRegexRules: RegexRule[] = [
             url: 'https://prirucka.ujc.cas.cz/?id=880 ',
             label: 'IJP'
         }],
-        search: new RegExp('⌀\u{0020}?(' + cislice + ')', 'g'),
-        replace: '⌀\u{00A0}$1'
+        search: new RegExp('\u{2300}\u{0020}?(' + cislice + ')', 'g'),
+        replace: '\u{2300}\u{00A0}$1'
     },
     {
         name: 'mezera u hashtagů',
@@ -287,14 +295,14 @@ export let autocorrectRegexRules: RegexRule[] = [
         name: 'nadbytečné mezery uvnitř uvozovek zleva',
         description: 'Nesprávné mezery',
         correctionLabel: 'Odstraňte nadbytečné mezery',
-        search: new RegExp('(' + interpunkceotviraci + ')\u{0020}?=(' + alnum + ')', 'g'),
+        search: new RegExp('(' + interpunkce_otviraci + ')\u{0020}(?=(' + alnum + '))', 'g'),
         replace: '$1'
     },
     {
         name: 'nadbytečné mezery uvnitř uvozovek zprava',
         description: 'Nesprávné mezery',
         correctionLabel: 'Odstraňte nadbytečné mezery',
-        search: new RegExp('(' + alnum + interpunkce + '?)\u{0020}(' + interpunkcezaviraci + ')', 'g'),
+        search: new RegExp('(' + alnum + interpunkce + '?)\u{0020}(' + interpunkce_zaviraci + ')', 'g'),
         replace: '$1$2'
     },
     {
@@ -305,7 +313,7 @@ export let autocorrectRegexRules: RegexRule[] = [
             url: 'https://prirucka.ujc.cas.cz/?id=163',
             label: 'IJP'
         }],
-        search: new RegExp('(' + alnum + ')(' + interpunkceotviraci + ')(' + alnum + ')', 'g'),
+        search: new RegExp('(' + alnum + ')(' + interpunkce_otviraci + ')(' + alnum + ')', 'g'),
         replace: '$1\u{0020}$2$3'
     },
     {
@@ -316,7 +324,7 @@ export let autocorrectRegexRules: RegexRule[] = [
             url: 'https://prirucka.ujc.cas.cz/?id=163',
             label: 'IJP'
         }],
-        search: new RegExp('(' + alnum + interpunkce + '?)(' + interpunkcezaviraci + ')(' + alnum + ')', 'g'),
+        search: new RegExp('(' + alnum + interpunkce + '?)(' + interpunkce_zaviraci + ')(' + alnum + ')', 'g'),
         replace: '$1$2\u{0020}$3'
     },
     /*{ //TODO
@@ -406,7 +414,7 @@ export let highlightRegexRules: RegexRule[] = [{
 {
     name: 'chybějící nezlomitelná mezera před znakem %, ‰',
     description: 'Nesprávná mezera',
-    correctionLabel: 'Změňte běžnou mezislovní mezeru na nerozdělitelnou',
+    correctionLabel: 'Změňte běžnou mezislovní mezeru na pevnou',
     about:[{
         url: 'https://prirucka.ujc.cas.cz/?id=785#nadpis3',
         label: 'IJP'
@@ -417,7 +425,7 @@ export let highlightRegexRules: RegexRule[] = [{
 {
     name: 'chybějící nezlomitelné mezery před znakem ° s udanou teplotní stupnicí',
     description: 'Nesprávná mezera',
-    correctionLabel: 'Změňte běžnou mezislovní mezeru na nerozdělitelnou',
+    correctionLabel: 'Změňte běžnou mezislovní mezeru na pevnou',
     about:[{
         url: 'https://prirucka.ujc.cas.cz/?id=785#nadpis3',
         label: 'IJP'
@@ -432,7 +440,7 @@ export let highlightRegexRules: RegexRule[] = [{
 {
     name: 'nezlomitelná mezera a odstranění znaku stupně u Kelvinovy teplotní stupnice',
     description: 'Nesprávná mezera',
-    correctionLabel: 'Změňte běžnou mezislovní mezeru na nerozdělitelnou',
+    correctionLabel: 'Změňte běžnou mezislovní mezeru na pevnou',
     about:[{
         url: 'https://prirucka.ujc.cas.cz/?id=785#nadpis3',
         label: 'IJP'
@@ -480,7 +488,7 @@ export let highlightRegexRules: RegexRule[] = [{
 {
     name: 'nezlomitelné mezery před znaménky = a + a po nich v obecných, nematematických zápisech',
     description: 'Nesprávná mezera',
-    correctionLabel: 'Změňte běžnou mezislovní mezeru na nerozdělitelnou',
+    correctionLabel: 'Změňte běžnou mezislovní mezeru na pevnou',
     about:[{
         url: 'https://prirucka.ujc.cas.cz/?id=785#nadpis4',
         label: 'IJP'
@@ -536,7 +544,7 @@ export let highlightRegexRules: RegexRule[] = [{
         url: 'https://prirucka.ujc.cas.cz/?id=162',
         label: 'IJP'
     }],
-    search: new RegExp('(^|\u{0020}|' + interpunkceotviraci + ')' + uvozovky + '(' + alnum + ')', 'g'),
+    search: new RegExp('(^|\u{0020}|' + interpunkce_otviraci + ')' + uvozovky + '(' + alnum + ')', 'g'),
     replace: '$1„$2'
 },
 {
@@ -547,7 +555,7 @@ export let highlightRegexRules: RegexRule[] = [{
         url: 'https://prirucka.ujc.cas.cz/?id=162',
         label: 'IJP'
     }],
-    search: new RegExp('(' + alnum + interpunkce + '?)' + uvozovky + '(\u{0020}|' + interpunkcezaviraci + ')', 'g'),
+    search: new RegExp('(' + alnum + interpunkce + '?)' + uvozovky + '(\u{0020}|' + interpunkce_zaviraci + ')', 'g'),
     replace: '$1“$2'
 },
 {
@@ -558,7 +566,7 @@ export let highlightRegexRules: RegexRule[] = [{
         url: 'https://prirucka.ujc.cas.cz/?id=165',
         label: 'IJP'
     }],
-    search: new RegExp('(' + pismena + interpunkcezaviraci + '?)\u{0020}[-–—](' + alnum + ')', 'g'),
+    search: new RegExp('(' + pismena + interpunkce_zaviraci + '?)\u{0020}[-–—](' + alnum + ')', 'g'),
     replace: '$1\u00A0–\u{0020}$2'
 },
 {
@@ -569,7 +577,7 @@ export let highlightRegexRules: RegexRule[] = [{
         url: 'https://prirucka.ujc.cas.cz/?id=165',
         label: 'IJP'
     }],
-    search: new RegExp('(' + pismena + interpunkcezaviraci + '?)[-–—]\u{0020}(' + alnum + ')', 'g'),
+    search: new RegExp('(' + pismena + interpunkce_zaviraci + '?)[-–—]\u{0020}(' + alnum + ')', 'g'),
     replace: '$1\u00A0–\u{0020}$2'
 },
 /*{//TODO
